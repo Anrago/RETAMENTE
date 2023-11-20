f8112d9e632df9ce3ab61158462b3674728453f6
Kevin <keevin-21@users.noreply.github.com>
2023-11-20 15:41:41 -0700

    new file:   assets/menuMusic.mp3 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include "raylib.h"
 
+void playMusic(Music music);
+
+float timePlayed = 0.0f;
+
 int main(void)
 {
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "Menu con Background - Raylib");
+    InitAudioDevice();
 
-    // Estructura del menu
     enum MenuOption
     {
         START_GAME,
@@ -17,10 +21,16 @@ int main(void)
 
     int selectedOption = START_GAME;
 
-    Texture2D background = LoadTexture("assets/bg_menu.jpg");
+    Music menuMusic = LoadMusicStream("assets/menuMusic.mp3");
+    Image bg_image = LoadImage("assets/bg_menu.png");
+    Texture2D background = LoadTextureFromImage(bg_image);
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        playMusic(menuMusic);
+
         if (IsKeyPressed(KEY_DOWN))
         {
             selectedOption = (selectedOption + 1) % 3;
@@ -48,23 +58,33 @@ int main(void)
             }
         }
 
-        // Background
-        DrawTexture(background, 0, 0, WHITE);
+        BeginDrawing();
 
-        // Menu
+        DrawTextureRec(background, (Rectangle){0, 0, screenWidth, screenHeight}, (Vector2){0, 0}, WHITE);
+
         DrawText("Retamente", (screenWidth - MeasureText("Retamente", 40)) / 2, 20, 40, DARKGRAY);
 
         DrawText(selectedOption == START_GAME ? "> Iniciar Juego" : "Iniciar Juego", (screenWidth - MeasureText("Iniciar Juego", 20)) / 2, screenHeight / 2 - 20, 20, DARKGRAY);
         DrawText(selectedOption == OPTIONS ? "> Opciones" : "Opciones", (screenWidth - MeasureText("Opciones", 20)) / 2, screenHeight / 2, 20, DARKGRAY);
         DrawText(selectedOption == EXIT ? "> Salir" : "Salir", (screenWidth - MeasureText("Salir", 20)) / 2, screenHeight / 2 + 20, 20, DARKGRAY);
-
-        BeginDrawing();
         EndDrawing();
     }
 
     UnloadTexture(background);
-
+    UnloadImage(bg_image);
+    UnloadMusicStream(menuMusic);
     CloseWindow();
 
     return 0;
+}
+
+void playMusic(Music music)
+{
+    PlayMusicStream(music);
+    UpdateMusicStream(music);
+
+    timePlayed = GetMusicTimePlayed(music) / GetMusicTimeLength(music);
+
+    if (timePlayed >= 0.99f)
+        timePlayed = 1.0f;
 }
