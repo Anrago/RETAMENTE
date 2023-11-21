f91ade4e04fd7e8a112dd49a4a7f8d6532ac95e6
Anrago <141792891+Anrago@users.noreply.github.com>
2023-11-21 11:18:44 -0800

    modified:   build/RetaMente.exe modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <math.h>
 
 const int screenWidth = 1280;
 const int screenHeight = 720;
@@ -35,8 +36,47 @@ void OptionsDraw();
 void CreditsUpdate();
 void CreditsDraw();
 
+
+
+
+typedef struct
+{
+    float startAngle;
+    float endAngle;
+    Color color;
+} RouletteSector;
+
+// Dibujar ruleta
+void DrawRoulette(RouletteSector *sectors, int sectorCount, float rotation)
+{
+    float radius = 200.0f;
+    Vector2 center = {screenWidth / 2.0f, screenHeight / 2.0f};
+
+    for (int i = 0; i < sectorCount; i++)
+    {
+        DrawSector(center, radius, sectors[i].startAngle + rotation, sectors[i].endAngle + rotation, sectors[i].color);
+    }
+
+    DrawCircle(center.x, center.y, radius - 10, RAYWHITE);
+}
+
+// Dibujar cada sector de la ruleta
+void DrawSector(Vector2 center, float radius, float startAngle, float endAngle, Color color)
+{
+    DrawRing(center, radius, radius - 10, startAngle, endAngle, 10, color);
+}
+
+
+
+
+
+
+
+
+
 int main(void)
 {
+    
     InitWindow(screenWidth, screenHeight, "retamente");
     InitAudioDevice();
 
@@ -62,7 +102,7 @@ int main(void)
             MenuDraw(background);
             break;
         case START_GAME:
-            currentScene = START_GAME;
+            StartGameUpdate();
             break;
         case OPTIONS:
             OptionsUpdate();
@@ -145,6 +185,70 @@ void MenuDraw(Texture2D background)
 
 void StartGameUpdate()
 {
+     Texture2D arrowTexture = LoadTexture("assets/Flecha.png");
+
+    // Definir los sectores de la ruleta
+    int sectorCount = 2;
+    RouletteSector sectors[2] = {
+        {0, 180, RED},
+        {180, 360, BLUE}
+        // {90, 135, GREEN},
+        // {135, 180, YELLOW},
+        // {180, 225, ORANGE},
+        // {225, 270, PINK},
+        // {270, 315, SKYBLUE},
+        // {315, 360, PURPLE},
+    };
+
+    float rotationSpeed = 0.0f;
+    float rotation = 0.0f;
+    bool spinning = false;
+
+    while (!WindowShouldClose())
+    {
+        // Actualizar
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            spinning = true;
+            rotationSpeed = 20.0f + GetRandomValue(-5, 5); // Velocidad de rotación más rápida y un poco de variación
+        }
+
+        if (spinning)
+        {
+            rotation += rotationSpeed;
+            rotationSpeed *= 0.99f;
+
+            if (rotationSpeed < 0.1f)
+            {
+                spinning = false;
+                rotationSpeed = 0.0f;
+            }
+        }
+
+        // Dibujar
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawRoulette(sectors, sectorCount, rotation);
+
+        // Dibujar flecha indicando la posición
+        Vector2 arrowPosition = {screenWidth / 2.0f - arrowTexture.width / 2, screenHeight / 2.0f - 230.0f};
+        DrawTextureEx(arrowTexture, arrowPosition, 0.0f, 1.0f, WHITE);
+
+        // Obtener el color de la ruleta en la posición de la flecha
+        Color arrowColor = GetPixelColor((int)arrowPosition.x + arrowTexture.width / 2, (int)arrowPosition.y + arrowTexture.height / 2);
+
+        // Comprobar si el color es ROJO (puedes cambiar esto según tu diseño de ruleta)
+        if (arrowColor.r == 255 && arrowColor.g == 0 && arrowColor.b == 0)
+        {
+            DrawText("Arrow is on RED!", 10, 10, 20, RED);
+        }
+
+        EndDrawing();
+    }
+
+    // De-inicialización
+    UnloadTexture(arrowTexture);
 }
 
 void StartGameDraw()
